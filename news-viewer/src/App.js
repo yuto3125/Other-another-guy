7cede27c8af851f8abc740c7fb098224bea47556
yuto3125 <51585533+yuto3125@users.noreply.github.com>
2020-07-26 13:01:15 +0900

    with async/await

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,10 +3,15 @@ import axios from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1'
+      );
       setData(response.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div className="App">
