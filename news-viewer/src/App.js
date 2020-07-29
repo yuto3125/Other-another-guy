fbdfdabab047c071255dd3628b554f8a73502d35
yuto3125 <51585533+yuto3125@users.noreply.github.com>
2020-07-29 19:52:18 +0900

    Finished

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,28 +1,38 @@
-import React, { useState } from 'react';
-import axios from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/axios';
+// import React, { useState } from 'react';
+import React from 'react';
+// import React, { useState, useCallback } from 'react';
+// import axios from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/axios';
+import { Route } from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1'
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       'http://newsapi.org/v2/top-headlines?country=kr&apiKey=fc59d8e67e2a4729bc9fa464f388240f'
+  //     );
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  // const [category, setCategory] = useState('all');
+  // const onSelect = useCallback(category => setCategory(category), []);
   return (
-    <div className="App">
-      <div className="import_btn">
-        <button onClick={onClick}>Import</button>
-      </div>
-      {data && (
-        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
-      )}
-    </div>
+    // <>
+    //   <Categories category={category} onSelect={onSelect} />
+    //   <NewsList category={category} />
+    // </>
+    <Route path="/:category?" component={NewsPage} />
   );
+  // (<div className="App">
+  //   <div className="import_btn">
+  //     <button onClick={onClick}>Import</button>
+  //   </div>
+  //   {data && (
+  //     <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
+  //   )}
+  // </div>);
 };
-
 export default App;
---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,66 @@
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
+import styled from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/styled-components';
+import axios from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/axios';
+import NewsItem from './NewsItem';
+import { usePromise } from '../lib/usePromise';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  // const [articles, setArticles] = useState(null);
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const query = category === 'all' ? '' : `&category=${category}`;
+  //       const response = await axios.get(
+  //         `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=fc59d8e67e2a4729bc9fa464f388240f`
+  //       );
+  //       setArticles(response.data.articles);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, [category]);
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category};`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=fc59d8e67e2a4729bc9fa464f388240f`
+    );
+  }, [category]);
+
+  if (loading) {
+    return <NewsListBlock>Still Loading...</NewsListBlock>;
+  }
+  if (!response) {
+    return null;
+  }
+  if (error) {
+    return <NewsListBlock>Error has occured!</NewsListBlock>;
+  }
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+export default NewsList;
